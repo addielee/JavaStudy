619e0552349392629298ea3e7e007e38af1d1509
Addie Lee <42773702+addielee@users.noreply.github.com>
2018-08-30 09:46:53 +0900

    Update 0829_Example01.java

--- 0829_Example01.java
+++ 0829_Example01.java
@@ -52,6 +52,7 @@ class Example01{
 
 	   //2번 : switch문 사용
 	   switch((int)avg/10){
+		   case 10 :
 		   case 9 : grade='A'; break;
 		   case 8 : grade='B'; break;
 		   case 7 : grade='C'; break;
